f5dc03cf9e1968dbb70bdc8a7327df17bc386bbf
shihwebog <49914528+shihwebog@users.noreply.github.com>
2026-03-07 17:26:04 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,6 +1,6 @@
 import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
-const pageConfig = {
+const pageConfig: PageConfig = {
   // Title for your status page
   title: "hxd Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
@@ -11,8 +11,7 @@ const pageConfig = {
   ],
 }
 
-const workerConfig = {
-  kvWriteCooldownMinutes: 3,
+const workerConfig: WorkerConfig = {
   monitors: [
     {
       id: 'Web',
@@ -20,8 +19,15 @@ const workerConfig = {
       method: 'GET',
       target: 'https://blog.stoh.cc',
         tooltip: 'This is a tooltip for this monitor',
-  // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-  statusPageLink: 'https://example.com',
+      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
+      statusPageLink: 'https://blog.stoh.cc',
+      expectedCodes: [200],
+      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
+      timeout: 10000,
+      // [OPTIONAL] headers to be sent
+      headers: {
+        'User-Agent': 'Uptimeflare',
+        Authorization: 'Bearer YOUR_TOKEN_HERE',
     },
 
  {
@@ -29,9 +35,16 @@ const workerConfig = {
       name: 'Social',
       method: 'GET',
       target: 'https://social.stoh.cc',
-     tooltip: 'This is a tooltip for this monitor',
-  // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-  statusPageLink: 'https://example.com',
+      tooltip: 'This is a tooltip for this monitor',
+     // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
+      statusPageLink: 'https://social.stoh.cc',
+      expectedCodes: [200],
+      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
+      timeout: 10000,
+      // [OPTIONAL] headers to be sent
+      headers: {
+        'User-Agent': 'Uptimeflare',
+        Authorization: 'Bearer YOUR_TOKEN_HERE', 
     },
 
     {
@@ -39,9 +52,10 @@ const workerConfig = {
       name: 'Main',
       method: 'TCP_PING',
       target: '137.175.126.187:22103',
-        tooltip: 'This is a tooltip for this monitor',
+      tooltip: 'This is a tooltip for this monitor',
   // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-  statusPageLink: 'https://example.com',
+  // statusPageLink: 'https://example.com',
+      timeout: 5000,
     },
 
     {
@@ -51,7 +65,8 @@ const workerConfig = {
       target: '72.11.140.72:22103',
         tooltip: 'This is a tooltip for this monitor',
   // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-  statusPageLink: 'https://example.com',
+ // statusPageLink: 'https://example.com',
+      timeout: 5000,
     },
    
     {
@@ -61,7 +76,8 @@ const workerConfig = {
       target: '131.186.32.166:22103',
         tooltip: 'This is a tooltip for this monitor',
   // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-  statusPageLink: 'https://example.com',
+  // statusPageLink: 'https://example.com',
+      timeout: 5000,
     },
 
     {
@@ -71,37 +87,31 @@ const workerConfig = {
       target: '168.138.201.51:22103',
         tooltip: 'This is a tooltip for this monitor',
   // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-  statusPageLink: 'https://example.com',
+  // statusPageLink: 'https://example.com',
+      timeout: 5000,
     },
     
   ],
-  callbacks: {
-    onStatusChange: async (
-      env: any,
-      monitor: any,
-      isUp: boolean,
-      timeIncidentStart: number,
-      timeNow: number,
-      reason: string,
-    ) => {
-      // This callback will be called when there's a status change for any monitor
-      // Write any Typescript code here
 
-      // This will not follow the grace period settings and will be called immediately when the status changes
-      // You need to handle the grace period manually if you want to implement it
-    },
-    onIncident: async (
-      env: any,
-      monitor: any,
-      timeIncidentStart: number,
-      timeNow: number,
-      reason: string,
-    ) => {
-      // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
-      // Write any Typescript code here
-    },
+const maintenances: MaintenanceConfig[] = [
+  {
+    // [Optional] Monitor IDs to be affected by this maintenance
+    monitors: ['foo_monitor', 'bar_monitor'],
+    // [Optional] default to "Scheduled Maintenance" if not specified
+    title: 'Test Maintenance',
+    // Description of the maintenance, will be shown at status page
+    body: 'This is a test maintenance, server software upgrade',
+    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
+    start: '2020-01-01T00:00:00+08:00',
+    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
+    // if not specified, the maintenance will be considered as on-going
+    end: '2050-01-01T00:00:00+08:00',
+    // [Optional] color of the maintenance alert at status page, default to "yellow"
+    color: 'blue',
   },
-}
+]
 
-// Don't forget this, otherwise compilation fails.
-export { pageConfig, workerConfig }
+// Don't edit this line
+export { maintenances, pageConfig, workerConfig }
+
+  
